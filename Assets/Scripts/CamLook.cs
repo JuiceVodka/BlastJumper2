fb1fac4523e70551f06737f60fd6260f7d2a4e6a
JuiceVodka <nikolajsok@gmail.com>
2022-01-08 18:33:06 +0100

    rocket knockback and shooting, scene, sound, particle effects

--- Assets/Scripts/CamLook.cs
+++ Assets/Scripts/CamLook.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class CamLook : MonoBehaviour
 {
 
+    public GameObject rockt;
+    public GameObject shootPoint;
+    float timeStamp;
+    float shootingCooldown = 1.0f;
+    
     private float yaw = 0.0f;
     private float pitch = 0.0f;
     [Range(0, 1000)] public float sensitivity = 300.0f;
@@ -13,6 +18,7 @@ public class CamLook : MonoBehaviour
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
+        timeStamp = Time.time;
     }
 
     // Update is called once per frame
@@ -25,6 +31,29 @@ public class CamLook : MonoBehaviour
 
         transform.localRotation = Quaternion.Euler(pitch, 0.0f, 0.0f);
         plyr.Rotate(new Vector3(0.0f, yaw, 0.0f));
+        
+        //raketa
+        if (Input.GetKeyDown(KeyCode.Mouse0) && timeStamp <= Time.time) {
+       	Shoot();
+       	timeStamp = Time.time + shootingCooldown;
+      	}
 
+    }
+    
+    void Shoot()
+    {
+    	RaycastHit hit;
+    	
+    	Quaternion fireRotation = Quaternion.LookRotation(transform.forward);
+    	
+    	
+    	if (Physics.Raycast(transform.position, fireRotation * Vector3.forward, out hit, Mathf.Infinity))
+    	{
+    		GameObject tempBullet = Instantiate(rockt, shootPoint.transform.position, fireRotation);
+    		tempBullet.GetComponent<rocket>().hitPoint = hit.point;
+    	}else{
+            GameObject tempBullet = Instantiate(rockt, shootPoint.transform.position, fireRotation);
+            tempBullet.GetComponent<rocket>().hitPoint = shootPoint.transform.position + (fireRotation * Vector3.forward)*100;
+        }
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     float speed;
+    public bool crouched = false;
+    float mass = 1f;
+    Vector3 impactVector = Vector3.zero;
+    float verticalVelocity = 0f;
+    float negSinus;
 
     public CharacterController chrCntrl;
     Vector3 move = new Vector3(0.0f, 0.0f, 0.0f);
@@ -16,29 +21,45 @@ public class PlayerMovement : MonoBehaviour
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
     bool grnd;
+    bool grndPrev;
 
     float jmpH = 3.0f;
 
 
     public Animator movement;
+    public Animator shooting;
 
+    float timeStamp;
+    float shootingCooldown = 0.7f;
+
+    public AudioSource asrc;
+    public AudioClip shoot;
+    public AudioClip crunch;
     // Start is called before the first frame update
     void Start()
     {
-        
+        timeStamp = Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {   
+    	grndPrev = grnd;
         //ground check
         grnd = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        
+        if (grndPrev == false && grnd == true) {
+            impactVector = Vector3.zero;
+        }
+        
 
         //slow walk and movement
         if(Input.GetKey(KeyCode.LeftShift)){
+            movement.SetBool("Slow", true);
             speed = 3.0f;
         }else{
             speed = 10.0f;
+            movement.SetBool("Slow", false);
         }
 
         float ha = Input.GetAxis("Horizontal");
@@ -49,19 +70,40 @@ public class PlayerMovement : MonoBehaviour
 
         //jumping
         if(Input.GetButtonDown("Jump") && grnd){
-            Debug.Log("skacemo");
             spd.y = Mathf.Sqrt(jmpH * -2 * grav);
         }
 
         //gravity
         if(grnd && spd.y < 0){
+            if(spd.y < -20){
+                asrc.PlayOneShot(crunch, 0.5f*GlobalTimer.sliderValue);
+            }
             spd.y = -2.0f;
         }else {
             spd.y += grav * Time.deltaTime;
         }
-        chrCntrl.Move(spd * Time.deltaTime);
+        
+        //Debug.Log("impVec= " + impactVector);
+        /*if (impactVector[2] > 0) {
+            impactVector[2] = impactVector[2] * (-1);
+        }*/
+        
+        chrCntrl.Move((spd + impactVector) * Time.deltaTime);
+        
 
         animateMotion(ha, va);
+
+        if(Input.GetMouseButtonDown(0) && timeStamp <= Time.time){
+            shooting.SetTrigger("Active");
+            timeStamp = Time.time + shootingCooldown;
+            asrc.PlayOneShot(shoot, 0.3f*GlobalTimer.sliderValue);
+        }
+
+        if(grnd){
+            chrCntrl.stepOffset = 0.5f;
+        }else{
+            chrCntrl.stepOffset = 0.0f;
+        }
     }
 
     private void animateMotion(float ha, float va){
@@ -69,12 +111,34 @@ public class PlayerMovement : MonoBehaviour
             //hoja
             if(ha == 0 && va == 0){
                 movement.SetFloat("Mode", 0.0f, 0.1f, Time.deltaTime);
+            }else if(va < 0){
+                movement.SetFloat("Mode", 0.33f, 0.1f, Time.deltaTime);
             }else{
-                movement.SetFloat("Mode", 0.5f, 0.1f, Time.deltaTime);
+                movement.SetFloat("Mode", 0.66f, 0.1f, Time.deltaTime);
             }
         }else{
             //skacemo oz letimo
             movement.SetFloat("Mode", 1.0f, 0.1f, Time.deltaTime);
         }
+    }
+    
+    public void AddImpact(float impactForce, Vector3 direction){
+        impactVector = Vector3.zero;
+        verticalVelocity = impactForce;
+        impactVector = direction;
+        impactVector.Normalize();
+        impactVector.y = 0f;
+//           impactVector *= impactForce * Mathf.Sin(Vector3.Angle(transform.up, direction)) / (0.5f * mass); 
+    	if (Mathf.Sin(Vector3.Angle(transform.up, direction)) > 0) {
+    	   negSinus = -1 * Mathf.Sin(Vector3.Angle(transform.up, direction));
+    	} 
+    	else {
+    	   negSinus = Mathf.Sin(Vector3.Angle(transform.up, direction));
+    	}
+        impactVector *= impactForce * negSinus / (0.5f * mass); 
+
+        //Debug.Log("inside sinus = " + Mathf.Sin(Vector3.Angle(transform.up, direction)));
+        spd.y = impactForce / mass;
+        
     }
 }
